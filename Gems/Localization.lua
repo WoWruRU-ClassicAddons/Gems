9c8901992245d5ba98c0466092f073d1d39798fd
M2us <Dead_Maus@mail.ru>
2017-06-12 21:08:34 +0300

    Added Gems with support ruRU locale

---
+++ Gems/Localization.lua
@@ -0,0 +1,34 @@
+if (GetLocale() == "ruRU") then
+	-- Russian localization by Lichery
+	
+	ADDON_LOADED = "Val's Gems загружен. Введите |cffff0000/gems|r для игры или |cffff0000/gems help|r для инструкций."
+	MSG_0 = "Помощь для Val's Gems"
+	MSG_1 = "Овладеть этой игрой легко - соедините 4 одинакового камня по горизонтали, вертикали или по диагонали."
+	MSG_2 = "Используйте кнопки '<' и '>', чтобы переместить падающие камни влево или вправо, и кнопку 'v', чтобы опустить камень вниз."
+	COMPLEXITY_0 = "Легко"
+	COMPLEXITY_1 = "Средне"
+	COMPLEXITY_2 = "Сложно"
+	COMPLEXITY_3 = "Безумие"	
+	SCORE = "Счет"
+	PAUSE = "Пауза"
+	-- some strings for the key bindings
+	BINDING_NAME_GEMS_LEFT = "Двигать влево";
+	BINDING_NAME_GEMS_RIGHT = "Двигать вправо";
+	BINDING_NAME_GEMS_DOWN = "Двигать вниз";
+else
+	ADDON_LOADED = "Val's Gems loaded. Type |cffff0000/gems|r to play or |cffff0000/gems help|r for instructions."
+	MSG_0 = "Help for Val's Gems"
+	MSG_1 = "The premise of this game is easy - connect 4 of the same gem horizontally, vertically, or diagonally."
+	MSG_2 = "Use the '<' and '>' buttons to move the falling gems left or right, and the 'v' button to drop the gem to the bottom."
+	COMPLEXITY_0 = "Easy"
+	COMPLEXITY_1 = "Medium"
+	COMPLEXITY_2 = "Hard"
+	COMPLEXITY_3 = "Crazy"
+	SCORE = "Score"
+	PAUSE = "Pause"
+	-- some strings for the key bindings
+	BINDING_HEADER_GEMS = "Val's Gems";
+	BINDING_NAME_GEMS_LEFT = "Move Left";
+	BINDING_NAME_GEMS_RIGHT = "Move Right";
+	BINDING_NAME_GEMS_DOWN = "Move Down";
+end
